646207d6efaa0af3febe1b947d7867cd903dde82
Sage Liu <sagectliu@gmail.com>
2022-11-26 16:17:12 -0600

    test

--- reply.py
+++ reply.py
@@ -1,32 +1,32 @@
 import random
 
-from newspaper import Article
-import string
-import nltk
-from sklearn.feature_extraction.text import CountVectorizer 
-from sklearn.metrics.pairwise import cosine_similarity 
-import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
+# from newspaper import Article
+# import string
+# import nltk
+# from sklearn.feature_extraction.text import CountVectorizer 
+# from sklearn.metrics.pairwise import cosine_similarity 
+# import numpy as np
+# import warnings
+# warnings.filterwarnings('ignore')
 
-#Download the punkt package
-nltk.download('punkt', quiet = True)
+# #Download the punkt package
+# nltk.download('punkt', quiet = True)
 
-#Get the article
-article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
-article.download()
-article.parse()
-article.nlp()
-corpus = article.text 
+# #Get the article
+# article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
+# article.download()
+# article.parse()
+# article.nlp()
+# corpus = article.text 
 
-#print(corpus)
+# #print(corpus)
 
-#Tokenization
-text = corpus
-sentence_list = nltk.sent_tokenize(text)  #A list of sentences
+# #Tokenization
+# text = corpus
+# sentence_list = nltk.sent_tokenize(text)  #A list of sentences
 
-#Print the list of sentences
-#print(sentence_list)
+# #Print the list of sentences
+# #print(sentence_list)
 
 class Reply:
     
@@ -64,33 +64,33 @@ class Reply:
     #     return list_index
 
     # Create the bots response
-    def bot_rsponse(self):
-        self.msg = self.msg.lower()
+    # def bot_rsponse(self):
+    #     self.msg = self.msg.lower()
         
-        sentence_list.append(self.msg)
-        bot_rsponse = ''
-        cm = CountVectorizer().fit_transform(sentence_list)
-        similarity_scores = cosine_similarity(cm[-1], cm)
-        similarity_scores_list = similarity_scores.flatten()
-        index = index_sort(similarity_scores_list)
-        index = index[1:]
-        reponse_flag = 0
+    #     sentence_list.append(self.msg)
+    #     bot_rsponse = ''
+    #     cm = CountVectorizer().fit_transform(sentence_list)
+    #     similarity_scores = cosine_similarity(cm[-1], cm)
+    #     similarity_scores_list = similarity_scores.flatten()
+    #     index = index_sort(similarity_scores_list)
+    #     index = index[1:]
+    #     reponse_flag = 0
 
-        j = 0
-        for i in range(len(index)):
-            if similarity_scores_list[index[i]] > 0.0:
-                bot_rsponse = bot_rsponse+' '+sentence_list[index[i]]
-                reponse_flag = 1
-                j = j + 1
-            if j > 2:
-                break
+    #     j = 0
+    #     for i in range(len(index)):
+    #         if similarity_scores_list[index[i]] > 0.0:
+    #             bot_rsponse = bot_rsponse+' '+sentence_list[index[i]]
+    #             reponse_flag = 1
+    #             j = j + 1
+    #         if j > 2:
+    #             break
 
-        if reponse_flag == 0:
-            bot_rsponse = bot_rsponse+' '+"I apologize, I don't understand."
+    #     if reponse_flag == 0:
+    #         bot_rsponse = bot_rsponse+' '+"I apologize, I don't understand."
 
-        sentence_list.remove(self.msg)
+    #     sentence_list.remove(self.msg)
 
-        return bot_rsponse
+    #     return bot_rsponse
 
 
 
