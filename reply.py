b128ad0a28e3e9af6c1bf54fecb89e46435bb9c8
Sage Liu <sagectliu@gmail.com>
2022-11-26 13:19:03 -0600

    test reply

--- reply.py
+++ reply.py
@@ -3,18 +3,31 @@ class Reply:
         self.msg = msg
 
 
-    def message(self):
-        r = "Sorry, I don't understand."
+    # def message(self):
+    #     r = "Sorry, I don't understand."
     
 
-        if self.msg in ['Hello', 'hello']:
-            r = 'Hi'
-        elif self.msg == 'I have a question':
-            r = 'How can I help you?'
-        elif self.msg == 'Who are you?':
-            r = 'I am robot'
+    #     if self.msg in ['Hello', 'hello']:
+    #         r = 'Hi'
+    #     elif self.msg == 'I have a question':
+    #         r = 'How can I help you?'
+    #     elif self.msg == 'Who are you?':
+    #         r = 'I am robot'
 
-        return r
+    #     return r
 
-    def __str__(self):
-    	return self.msg
+    def message(self):
+        self.msg = self.msg.lower()
+
+        #Bots greeting response
+        bot_greetings = ['howdy', 'hi', 'hey', 'hello']
+        #User greeting
+        user_greetings = ['hi', 'hey', 'hello', 'greetings', 'wassup']
+
+        for word in self.msg.split():
+        	if word in user_greetings:
+        		r = random.choice(bot_greetings)
+        		return r
+
+
+
